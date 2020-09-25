01a5db639356c9fd0f7ef9d894cb84ef871e1a55
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2020-09-25 20:46:20 +0300

    Add lab1

---
+++ lab1-MAC-Address/installer/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace installer
+{
+    public class Program
+    {
+        static int iterCount = 1000;
+
+        static void Main(string[] args)
+        {
+            var b = GetUniqueValue();
+
+            var rng = RandomNumberGenerator.Create();
+            const KeyDerivationPrf Pbkdf2Prf = KeyDerivationPrf.HMACSHA1; // default for Rfc2898DeriveBytes
+            int Pbkdf2IterCount = 1000; // default for Rfc2898DeriveBytes
+            const int Pbkdf2SubkeyLength = 256 / 8; // 256 bits
+            const int SaltSize = 128 / 8; // 128 bits
+
+            var bhash = Hash(b, rng, Pbkdf2Prf, Pbkdf2IterCount, SaltSize, Pbkdf2SubkeyLength);
+            System.IO.File.WriteAllBytes("coolfile.txt", bhash);
+        }
+
+        #region unique data
+        private static string GetUniqueValue()
+        {
+            return GetMAC2();
+        }
+
+        private static string GetMAC()
+        {
+            var networks = NetworkInterface.GetAllNetworkInterfaces();
+            var activeNetworks = networks
+                .Where(ni => ni.OperationalStatus == OperationalStatus.Up && ni.NetworkInterfaceType != NetworkInterfaceType.Loopback);
+            var sortedNetworks = activeNetworks.OrderByDescending(ni => ni.Speed);
+            return sortedNetworks.First().GetPhysicalAddress().ToString();
+        }
+
+        private static string GetMAC2()
+        {
+            return NetworkInterface.GetAllNetworkInterfaces()
+                .Where(nic => nic.OperationalStatus == OperationalStatus.Up && nic.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+                .Select(nic => nic.GetPhysicalAddress().ToString())
+                .FirstOrDefault();
+        }
+        #endregion
+
+
+        #region hashing
+        private static byte[] Hash(string password, RandomNumberGenerator rng, KeyDerivationPrf prf, int iterCount, int saltSize, int numBytesRequested)
+        {
+            byte[] salt = new byte[saltSize];
+            rng.GetBytes(salt);
+            byte[] subkey = KeyDerivation.Pbkdf2(password, salt, prf, iterCount, numBytesRequested);
+
+            var outputBytes = new byte[13 + salt.Length + subkey.Length];
+            outputBytes[0] = 0x01; // format marker
+            WriteNetworkByteOrder(outputBytes, 1, (uint)prf);
+            WriteNetworkByteOrder(outputBytes, 5, (uint)iterCount);
+            WriteNetworkByteOrder(outputBytes, 9, (uint)saltSize);
+            Buffer.BlockCopy(salt, 0, outputBytes, 13, salt.Length);
+            Buffer.BlockCopy(subkey, 0, outputBytes, 13 + saltSize, subkey.Length);
+            return outputBytes;
+        }
+
+        public static bool VerifyCurrentMachine(byte[] hashedData)
+        {
+            string data = GetUniqueValue();
+            return VerifyHashed(hashedData, data);
+        }
+
+        private static bool VerifyHashed(byte[] hashedPassword, string password)
+        {
+            iterCount = default(int);
+
+            try
+            {
+                // Read header information
+                KeyDerivationPrf prf = (KeyDerivationPrf)ReadNetworkByteOrder(hashedPassword, 1);
+                iterCount = (int)ReadNetworkByteOrder(hashedPassword, 5);
+                int saltLength = (int)ReadNetworkByteOrder(hashedPassword, 9);
+
+                // Read the salt: must be >= 128 bits
+                if (saltLength < 128 / 8)
+                {
+                    return false;
+                }
+                byte[] salt = new byte[saltLength];
+                Buffer.BlockCopy(hashedPassword, 13, salt, 0, salt.Length);
+
+                // Read the subkey (the rest of the payload): must be >= 128 bits
+                int subkeyLength = hashedPassword.Length - 13 - salt.Length;
+                if (subkeyLength < 128 / 8)
+                {
+                    return false;
+                }
+                byte[] expectedSubkey = new byte[subkeyLength];
+                Buffer.BlockCopy(hashedPassword, 13 + salt.Length, expectedSubkey, 0, expectedSubkey.Length);
+
+                // Hash the incoming password and verify it
+                byte[] actualSubkey = KeyDerivation.Pbkdf2(password, salt, prf, iterCount, subkeyLength);
+
+
+                if (actualSubkey.Length != expectedSubkey.Length)
+                    return false;
+
+                for (int i = 0; i < actualSubkey.Length; i++)
+                    if (actualSubkey[i] != expectedSubkey[i])
+                        return false;
+
+                return true;
+                //return nFixedTimeEquals(actualSubkey, expectedSubkey);
+            }
+            catch
+            {
+                // This should never occur except in the case of a malformed payload, where
+                // we might go off the end of the array. Regardless, a malformed payload
+                // implies verification failed.
+                return false;
+            }
+        }
+
+        private static uint ReadNetworkByteOrder(byte[] buffer, int offset)
+        {
+            return ((uint)(buffer[offset + 0]) << 24)
+                | ((uint)(buffer[offset + 1]) << 16)
+                | ((uint)(buffer[offset + 2]) << 8)
+                | ((uint)(buffer[offset + 3]));
+        }
+
+        private static void WriteNetworkByteOrder(byte[] buffer, int offset, uint value)
+        {
+            buffer[offset + 0] = (byte)(value >> 24);
+            buffer[offset + 1] = (byte)(value >> 16);
+            buffer[offset + 2] = (byte)(value >> 8);
+            buffer[offset + 3] = (byte)(value >> 0);
+        }
+        #endregion
+
+    }
+}
---
+++ lab1-MAC-Address/program/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (CheckAuthority())
+                Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+
+        static bool CheckAuthority()
+        {
+            string filename = "coolfile.txt";
+            if (!File.Exists(filename))
+            {
+                Console.WriteLine("Please, run installer first.");
+                return false;
+            }
+
+            byte[] dataHash = File.ReadAllBytes(filename);
+            if (installer.Program.VerifyCurrentMachine(dataHash))
+                return true;
+            else
+                Console.WriteLine("Error. Different machine.");
+            return false;
+        }
+
+
+    }
+}
