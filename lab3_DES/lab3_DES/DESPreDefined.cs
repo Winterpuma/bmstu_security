b7eb117d1999a920e053e800af6a92e9696c77b5
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2020-11-25 02:25:41 +0300

    Add Feistel Function

--- lab3_DES/lab3_DES/DESPreDefined.cs
+++ lab3_DES/lab3_DES/DESPreDefined.cs
@@ -11,11 +11,18 @@ namespace lab3_DES
         private static string _rootDirectory = @"D:\GitHub\bmstu_security\lab3_DES\permutations\";
         public static int[] encoding;
         public static int[] decoding;
+        public static int[] expansion;
+        public static int[] finishP;
+        public static int[][][] sBlocks;
         
         static DESPreDefined()
         {
             encoding = ReadIntArrayFromFile(_rootDirectory + "encoding.txt");
             decoding = ReadIntArrayFromFile(_rootDirectory + "decoding.txt");
+            expansion = ReadIntArrayFromFile(_rootDirectory + "Expansion.txt");
+            finishP = ReadIntArrayFromFile(_rootDirectory + "finishP.txt");
+
+            ReadSBlocks(_rootDirectory + "SBlocks.txt");
         }
 
         /// <summary>
@@ -31,6 +38,36 @@ namespace lab3_DES
                 res[i] = Convert.ToInt32(splitedText[i]);
 
             return res;
+        }
+
+        /// <summary>
+        /// Считывает таблицу для S блоков
+        /// </summary>
+        private static void ReadSBlocks(string filename)
+        {
+            int nBlocks = 8;
+            int nLines = 4;
+            int nNums = 16;
+
+            sBlocks = new int[nBlocks][][];
+            string text = File.ReadAllText(filename);
+            string[] splittedByBlocks = text.Split('-');
+
+            for (int iBlock = 0; iBlock < nBlocks; iBlock++)
+            {
+                int[][] curBlock = new int[nLines][];
+                string[] splittedBlockByLine = splittedByBlocks[iBlock].Trim().Split('\n');
+
+                for (int iLine = 0; iLine < nLines; iLine++)
+                {
+                    string[] splittedLine = splittedBlockByLine[iLine].Trim().Split(' ');
+                    int[] curLine = new int[nNums];
+                    for (int iNum = 0; iNum < nNums; iNum++)
+                        curLine[iNum] = Convert.ToInt32(splittedLine[iNum]);
+                    curBlock[iLine] = curLine;
+                }
+                sBlocks[iBlock] = curBlock;
+            }
         }
     }
 }
--- lab3_DES/lab3_DES/DataOperations.cs
+++ lab3_DES/lab3_DES/DataOperations.cs
@@ -13,11 +13,93 @@ namespace lab3_DES
             bool[] newData = new bool[indexes.Length];
 
             for (int i = 0; i < indexes.Length; i++)
-            {
                 newData[i] = data[indexes[i] - 1];
-            }
 
             return new BitArray(newData);
+        }
+
+        /// <summary>
+        /// Функция Фейстеля
+        /// </summary>
+        /// <param name="right">Правый блок (32 бита)</param>
+        /// <param name="key">Ключ (48 бит)</param>
+        public static BitArray FeistelFunction(BitArray right, BitArray key)
+        {
+            // Расширение 
+            var curBitArray = Permutate(right, DESPreDefined.expansion);
+
+            // Сложение по модулю 2 с ключом
+            curBitArray.Xor(key);
+
+            // Преобразование S
+            int[] sTransformRes = GetTransformedS(curBitArray);
+            curBitArray = Convert4BitIntArrayToBitArray(sTransformRes);
+
+            // Перестановка P
+            curBitArray = Permutate(curBitArray, DESPreDefined.finishP);
+
+            return curBitArray;
+        }
+
+        /// <summary>
+        /// Получает преобразование для каждого (из восьми) 6 битового значения
+        /// в 4х битовое значение при помощи преобразования S
+        /// </summary>
+        private static int[] GetTransformedS(BitArray right)
+        {
+            int[] sTransformRes = new int[8];
+            for (int iBlock = 0; iBlock < 8; iBlock++)
+            {
+                int iCurBlockStart = iBlock * 6;
+
+                int a = 0;
+                a += right[iCurBlockStart] ? 1 : 0;
+                a += right[iCurBlockStart + 5] ? 2 : 0;
+
+                int b = 0;
+                b += right[iCurBlockStart + 1] ? 1 : 0;
+                b += right[iCurBlockStart + 2] ? 2 : 0;
+                b += right[iCurBlockStart + 3] ? 4 : 0;
+                b += right[iCurBlockStart + 4] ? 8 : 0;
+
+                sTransformRes[iBlock] = DESPreDefined.sBlocks[iBlock][a][b];
+            }
+            return sTransformRes;
+        }
+
+        /// <summary>
+        /// Преобразует массив чисел[0;15] в массив бит
+        /// </summary>
+        private static BitArray Convert4BitIntArrayToBitArray(int[] intArr)
+        {
+            BitArray res = new BitArray(intArr.Length * 4);
+
+            for (int iBlock = 0, iCurBlockStart = 0; iBlock < intArr.Length; iBlock++, iCurBlockStart += 4)
+            {
+                int curNum = intArr[iBlock];
+                if (curNum >= 16)
+                    throw new System.Exception("Передано число занимающее более чем 4 бита");
+
+                if (curNum >= 8)
+                {
+                    res[iCurBlockStart + 3] = true;
+                    curNum -= 8;
+                }
+                if (curNum >= 4)
+                {
+                    res[iCurBlockStart + 2] = true;
+                    curNum -= 4;
+                }
+                if (curNum >= 2)
+                {
+                    res[iCurBlockStart + 1] = true;
+                    curNum -= 2;
+                }
+                if (curNum == 1)
+                    res[iCurBlockStart] = true;
+            }
+
+            return res;
         }
     }
 }
